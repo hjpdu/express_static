f199922a77302f8146ab9fb136ef2ba46dd2f908
Harryson Pointdujour <Harryson Pointdujour>
2016-09-16 20:41:36 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var server = express();
+
+
+server.get('/', function(request, response){
+  response.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.listen(8080,function(){
+  console.log("Now listening on port...", 8080);
+
+  /*test this with node index.js in terminal*/
+});
